a7a544e5d6d7e192437fee94bd24a53c27d568ee
Josh Seyda <josh.seyda@gmail.com>
2018-03-26 16:52:47 -0400

    MVP needs met, experiment with side navigation,image  carousel, and timed modal pop up

---
+++ main.js
@@ -0,0 +1,14 @@
+ $(document).ready(function(){
+ 	function showModal(){
+ 		$('.modal').modal();
+ 		$('#modal1').modal('open');
+ 	}
+     setTimeout(showModal, 1000)
+  });
+
+   $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+    $(document).ready(function(){
+    $('.carousel').carousel();
+  });
